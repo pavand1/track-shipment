80512b618fe0957b2d8799dfb2218fb42fa60956
Pavan Divekar <pavan.divekar@yahoo.in>
2024-08-31 14:11:43 +0000

    email integrations for get in touch

--- client/src/components/common/cp-banner/CpHomeBanner.jsx
+++ client/src/components/common/cp-banner/CpHomeBanner.jsx
@@ -1,7 +1,42 @@
+import { useRef, useState } from 'react';
 import styles from './cpBanner.module.scss';
 import {mockData} from './CpBanner_data';
+import emailjs from '@emailjs/browser';
 
 const CpHomeBanner = () => {
+  const form = useRef();
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [phone1, setPhone1] = useState('');
+  const [phone2, setPhone2] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSendEmail = (e) => {
+    e.preventDefault();
+    if(!name || !email || !message || !phone1) {
+      setError('All fields are required, except the secondary phone number');
+      return;
+    }
+    emailjs
+      .sendForm('service_f5bl48g', 'template_agxy0jb', form.current, {
+        publicKey: 'UwYDiQ1gdS6YNU1C-',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          setError('');
+          setName('');
+          setEmail('');
+          setMessage('');
+          setPhone1('');
+          setPhone2('');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
   return (
     <section className={styles.section}>
         <div className={styles.left}>
@@ -10,30 +45,66 @@ const CpHomeBanner = () => {
           <button className={styles.button}>{mockData.buttonText}</button>
         </div>
         <div className={styles.right}>
-          <form className={styles.form}>
+          <form className={styles.form} ref={form} onSubmit={handleSendEmail} style={{position: 'relative'}}>
           <h2 className={styles.heading}>{mockData.form.heading}</h2>
-            {mockData.form.fields.map((field) => (
-              <div key={field.id} className={styles.formGroup}>
-                {field.type === 'textarea' ? (
-                  <textarea
-                    id={field.id}
-                    name={field.id}
-                    placeholder={field.placeholder}
+          <div className={styles.formGroup}>
+            <input
+                    type={"text"}
+                    id={"name"}
+                    name={"name"}
+                    placeholder={"Name"}
+                    required
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+            </div>
+            <div className={styles.formGroup}>
+            <input
+                    type={"email"}
+                    id={"email"}
+                    name={"email"}
+                    placeholder={"email"}
+                    required
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+            </div>
+            <div className={styles.formGroup}>
+            <input
+                    type={"number"}
+                    id={"phone1"}
+                    name={"phone"}
+                    placeholder={"primary phone number"}
+                    required
+                    value={phone1}
+                    onChange={(e) => setPhone1(e.target.value)}
+                  />
+            </div>
+            <div className={styles.formGroup}>
+            <input
+                    type={"number"}
+                    id={"phone2"}
+                    name={"phone 2"}
+                    placeholder={"secondary phone number"}
+                    value={phone2}
+                    onChange={(e) => setPhone2(e.target.value)}
+                  />
+            </div>
+            <div className={styles.formGroup}>
+            <textarea
+                    id={"message"}
+                    name={"message"}
+                    placeholder={"message"}
                     rows="4"
+                    required
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
                   />
-                ) : (
-                  <input
-                    type={field.type}
-                    id={field.id}
-                    name={field.id}
-                    placeholder={field.placeholder}
-                  />
-                )}
-              </div>
-            ))}
+            </div>
             <button type="submit" className={styles.submitButton}>
               {mockData.form.submitButtonText}
             </button>
+            {error && <p className={styles.error}>{error}</p>}
           </form>
         </div>
     </section>
